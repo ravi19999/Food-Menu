979aafb7d465ac88f047a7e88d416216b625e06f
ravi19999 <dahalravindra@gmail.com>
2020-12-10 08:59:19 +0545

    Completed project :boom:

--- js/index.js
+++ js/index.js
@@ -2,9 +2,12 @@ const breakfastPrice = [200, 300, 400, 500, 600];
 const lunchPrice = [400, 500, 600, 700, 800];
 const dinnerPrice = [150, 250, 350, 450, 550];
 
-const dropDownComponent = document.querySelector(".dropdown");
+serves = [2, 4, 3, 5, 6];
+time = [15, 25, 35, 40, 50];
 
-let dropDownElement = document.createElement("li");
+let dropDownComponent = document.querySelector(".dropdown");
+
+// let dropDownElement = document.createElement("li");
 
 let dropdownHTML = `<div class='dropdown-elements'>
                         <div class='dropdown-image'></div>
@@ -13,7 +16,7 @@ let dropdownHTML = `<div class='dropdown-elements'>
                             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium maiores hic exercitationem suscipit numquam labore modi! Nemo nihil ad sit molestiae dolorum, consectetur impedit sunt doloribus rem rerum tempora incidunt.</p>
                         </div>
                         <div class='span-container'>
-                            <span class='dropdown-span price'><br><br>Price <br> <strong class='large'>Rs.$100<strong></span>
+                            <span class='dropdown-span price'><br><br>Price <br> <strong class='large'>Rs. 150<strong></span>
                         </div>
                         
                     </div>
@@ -23,18 +26,121 @@ let dropdownHTML = `<div class='dropdown-elements'>
                     </span>
 `;
 
-breakfastClicked = document
-  .querySelector(".breakfast")
-  .addEventListener("click", (event) => {
-    for (let i = 0; i <= 5; i++) {}
-  });
-lunchClicked = document
-  .querySelector(".lunch")
-  .addEventListener("click", (event) => console.log(event.target.classList[1]));
-dinnerClicked = document
-  .querySelector(".dinner")
-  .addEventListener("click", (event) => console.log(event.target.classList[1]));
+// breakfastClicked =
 
-dropDownElement.innerHTML = dropdownHTML;
+document.querySelector(".lunch").addEventListener("click", () => {
+  dropDownComponent.innerHTML = "";
+  let foodType = "Lunch Item";
+  for (let i = 0; i < 5; i++) {
+    let dropDownElement = document.createElement("li");
+    let price = lunchPrice[i];
+    let image = lunch[i];
 
-dropDownComponent.appendChild(dropDownElement);
+    let dropdownHTML = `<div class='dropdown-elements'>
+                        <div class='dropdown-image' style="background-image:url(${image})"></div>
+                        <div class='dropdown-text'>
+                            <h1>${foodType}</h1>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium maiores hic exercitationem suscipit numquam labore modi! Nemo nihil ad sit molestiae dolorum, consectetur impedit sunt doloribus rem rerum tempora incidunt.</p>
+                        </div>
+                        <div class='span-container'>
+                            <span class='dropdown-span price'><br><br>Price <br> <strong class='large'>Rs. ${price}<strong></span>
+                        </div>
+                        
+                    </div>
+                    <span class='dropdown-footer'>
+                         <div class='span-item'>⏱ 15 mins</div>
+                         <div class='span-item'> 👯  2 serves</div> 
+                    </span>
+`;
+    dropDownElement.innerHTML = dropdownHTML;
+    dropDownComponent.appendChild(dropDownElement);
+  }
+});
+
+document.querySelector(".dinner").addEventListener("click", () => {
+  dropDownComponent.innerHTML = "";
+
+  let foodType = "Dinner Item";
+  for (let i = 0; i < 5; i++) {
+    let dropDownElement = document.createElement("li");
+    let price = dinnerPrice[i];
+    let image = dinner[i];
+    let dropdownHTML = `<div class='dropdown-elements'>
+                        <div class='dropdown-image' style="background-image:url(${image})"></div>
+                        <div class='dropdown-text'>
+                            <h1>${foodType}</h1>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium maiores hic exercitationem suscipit numquam labore modi! Nemo nihil ad sit molestiae dolorum, consectetur impedit sunt doloribus rem rerum tempora incidunt.</p>
+                        </div>
+                        <div class='span-container'>
+                            <span class='dropdown-span price'><br><br>Price <br> <strong class='large'>Rs. ${price}<strong></span>
+                        </div>
+                        
+                    </div>
+                    <span class='dropdown-footer'>
+                         <div class='span-item'>⏱ 40 mins</div>
+                         <div class='span-item'> 👯  4 serves</div> 
+                    </span>
+`;
+    dropDownElement.innerHTML = dropdownHTML;
+    dropDownComponent.appendChild(dropDownElement);
+  }
+});
+
+// dropDownElement.innerHTML = dropdownHTML;
+
+document.querySelector(".breakfast").addEventListener("click", () => {
+  dropDownComponent.innerHTML = "";
+
+  let foodType = "Breakfast Item";
+  for (let i = 0; i < 5; i++) {
+    let dropDownElement = document.createElement("li");
+    let price = breakfastPrice[i];
+    let image = breakfast[i];
+
+    let dropdownHTML = `<div class='dropdown-elements'>
+                        <div class='dropdown-image' style="background-image:url(${image})"></div>
+                        <div class='dropdown-text'>
+                            <h1>${foodType}</h1>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium maiores hic exercitationem suscipit numquam labore modi! Nemo nihil ad sit molestiae dolorum, consectetur impedit sunt doloribus rem rerum tempora incidunt.</p>
+                        </div>
+                        <div class='span-container'>
+                            <span class='dropdown-span price'><br><br>Price <br> <strong class='large'>Rs. ${price}<strong></span>
+                        </div>
+                        
+                    </div>
+                    <span class='dropdown-footer'>
+                         <div class='span-item'>⏱ 30 mins</div>
+                         <div class='span-item'> 👯  3 serves</div> 
+                    </span>
+`;
+    dropDownElement.innerHTML = dropdownHTML;
+    dropDownComponent.appendChild(dropDownElement);
+  }
+});
+
+let foodType = "Breakfast Item";
+for (let i = 0; i < 5; i++) {
+  let dropDownElement = document.createElement("li");
+  let price = breakfastPrice[i];
+  let image = breakfast[i];
+
+  let dropdownHTML = `<div class='dropdown-elements'>
+                        <div class='dropdown-image' style="background-image:url(${image})"></div>
+                        <div class='dropdown-text'>
+                            <h1>${foodType}</h1>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium maiores hic exercitationem suscipit numquam labore modi! Nemo nihil ad sit molestiae dolorum, consectetur impedit sunt doloribus rem rerum tempora incidunt.</p>
+                        </div>
+                        <div class='span-container'>
+                            <span class='dropdown-span price'><br><br>Price <br> <strong class='large'>Rs. ${price}<strong></span>
+                        </div>
+                        
+                    </div>
+                    <span class='dropdown-footer'>
+                         <div class='span-item'>⏱ 15 mins</div>
+                         <div class='span-item'> 👯  2 serves</div> 
+                    </span>
+`;
+  dropDownElement.innerHTML = dropdownHTML;
+  dropDownComponent.appendChild(dropDownElement);
+}
+// dropDownComponent.appendChild(dropDownElement);
